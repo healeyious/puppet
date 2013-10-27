07def1fbe45be8e70444db26f3f7a3948b62686b
Paul Healey <paulhealey@brighttalk.com>
2013-10-27 18:09:07 +0000

    Update Sample.pp

--- Sample.pp
+++ Sample.pp
@@ -40,30 +40,21 @@ file {'/etc/httpd/conf/httpd.conf':
 notify => Service ["httpd"],
 ensure => file,
 mode => 0644,
-source => '/root/payload/httpd.conf',
+source => '/root/httpd.payload',
 require => Package ["httpd"]
 }
 file {'/var/www/html/index.php':
 notify => Service ["httpd"],
 ensure => file,
 mode => 0644,
-source => '/root/payload/index.php',
+source => '/root/index.payload',
 require => Package ["httpd"]
-}
-file {'/var/www/html/phpMyAdmin':
-ensure => directory,
-recurse => true,
-purge => true,
-force => true,
-mode => 0766,
-source => '/root/payload/phpMyAdmin-4.0.8-all-languages',
-require => Package ["httpd"],
 }
 file {'/etc/sysconfig/iptables':
 notify => Service ["iptables"],
 ensure => file,
 mode => 0600,
-source => '/root/payload/iptables',
+source => '/root/iptables.payload',
 }
 service {'httpd':
 ensure => running,
@@ -76,8 +67,4 @@ enable => true,
 service {'mysqld':
 ensure => running,
 enable => true,
-}->
-exec { "mysqladmin -u root password apple":
-require => Package ["mysql-server"],
-path => ["/usr/bin"],
 }
